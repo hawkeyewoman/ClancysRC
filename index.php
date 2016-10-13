d89ce3fd10d2f4fd6119eaac55642bbc834d2cd4
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-13 14:05:30 -0600

    improvement on home page
    added picture, font to home page

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title></title>
+    <title>index</title>
     <?php require_once("lib/headUtils.php"); ?>
 
   </head>
   <body>
     <?php require_once("lib/partials/navbar.php");?>
 
+    <div class="jumbotron">
+      <h1 id="headText">Clancy's</h1>
+      <p id="headP">Irish and Sushi Restaurant</p>
+    </div>
   </body>
 </html>
