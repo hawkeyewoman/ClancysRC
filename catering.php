9707a8a890b6bf66147219dce9367a595a2ba0ae
rasmith78 <roselynsmith78@gmail.com>
2016-10-13 14:00:08 -0600

    made location page

--- catering.php
+++ catering.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Catering</title>
+      <?php require_once("lib/headUtils.php"); ?>
+    <link rel="stylesheet" href="lib/css/catering.css"/>
+
   </head>
   <body>
-
+    <?php require_once("lib/partials/navbar.php");?>
   </body>
+
 </html>
--- location.php
+++ location.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Location</title>
+      <?php require_once("lib/headUtils.php"); ?>
+      <link rel="stylesheet" href="lib/css/location.css"/>
+
   </head>
+
   <body>
+      <?php require_once("lib/partials/navbar.php");?>
+
+      <h1>Farmington Location</h1>
+
+      <div class="container">
+        <iframe class="center-block" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3196.9483184607116!2d-108.17252138471163!3d36.747811579959404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x873b8f6912a4bcd5%3A0x2642b42947bb5cc8!2sClancy&#39;s+Pub+an+Irish+Cantina!5e0!3m2!1sen!2sus!4v1475699514085" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
 
   </body>
 </html>
