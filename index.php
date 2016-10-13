bd6de0839184b2b46e6fe3568a5cb1699f03cea9
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-13 14:58:48 -0600

    added picture and text to home page
    made changes and added stuff

--- index.php
+++ index.php
@@ -12,5 +12,37 @@
       <h1 id="headText">Clancy's</h1>
       <p id="headP">Irish and Sushi Restaurant</p>
     </div>
+
+    <div class="container" id="center">
+      <div class="row margin-top">
+        <div class="col-md-3">
+          <img src="lib/images/sushi.jpg" class="img-responsive center-block" style="height: 300px; width: 300px"
+           alt="picture of sushi">
+          <h2>Sushi Bar</h2>
+          <p style="font-family: Paprika, cursive;">Visit our Sushi Bar today!  Hours Monday to Friday from
+          11am-2pm and 5pm-10pm.  Saturday hours are 11am-pm.  Sushi Bar is closed on Sundays.</p>
+        </div>
+
+    <div class="container" id="center">
+      <div class="row">
+        <div class="col-md-3">
+          <h3><i class="fa fa-map-marker" aria-hidden="true"></i> Address</h3>
+          <p id="adressP"> 2701 E 20th Farmington, NM 87402</p>
+
+        </div>
+
+        <div class="col-md-3">
+          <h3><i class="fa fa-phone" aria-hidden="true"></i> Phone</h3>
+          <p id="adressP"> 505-325-8176</p>
+        </div>
+
+        <div class="col-md-3">
+          <h3><i class="fa fa-clock-o" aria-hidden="true"></i> Hours</h3>
+          <p id="adressP"> 11:00am to 10:00pm daily</p>
+        </div>
+
+    </div>
+  </div>
+
   </body>
 </html>
--- lib/headUtils.php
+++ lib/headUtils.php
@@ -2,7 +2,7 @@
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
   rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
     crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css?family=Inknut+Antiqua|Molle:400i|Uncial+Antiqua"
+<link href="https://fonts.googleapis.com/css?family=Elsie|Inknut+Antiqua|Molle:400i|Mystery+Quest|Paprika|Uncial+Antiqua"
   rel="stylesheet">
 <link rel="stylesheet" href="lib/css/bootstrap.css"/>
 
