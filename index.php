e93152c50620da1ed0b19fa19b2c235d3d736b64
rasmith78 <roselynsmith78@gmail.com>
2016-10-13 12:41:43 -0600

    moved headutils and added css files

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title></title>
-    <?php require_once("lib/partials/headUtils.php"); ?>
+    <?php require_once("lib/headUtils.php"); ?>
   </head>
   <body>
     <?php require_once("lib/partials/navbar.php");?>
